f19c2571060bb7ca53eb818ab41d1d52155ba5c8
Gudrun <Gudrun.huszar@gmx.at>
2019-08-27 13:25:12 +0200

    Basic of Beacon scanning. Scanning runs, but no beacons/artefacts shown

--- app/src/main/java/com/example/gudrun/test/ArtInAreaActivity.java
+++ app/src/main/java/com/example/gudrun/test/ArtInAreaActivity.java
@@ -3,48 +3,96 @@ package com.example.gudrun.test;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
-import java.io.IOException;
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.OkHttpClient;
+import android.util.Log;
+
+
+import com.estimote.mustard.rx_goodness.rx_requirements_wizard.Requirement;
+import com.estimote.mustard.rx_goodness.rx_requirements_wizard.RequirementsWizardFactory;
+import com.estimote.proximity_sdk.api.EstimoteCloudCredentials;
+import com.estimote.proximity_sdk.api.ProximityObserver;
+import com.estimote.proximity_sdk.api.ProximityObserverBuilder;
+import com.estimote.proximity_sdk.api.ProximityZone;
+import com.estimote.proximity_sdk.api.ProximityZoneBuilder;
+import com.estimote.proximity_sdk.api.ProximityZoneContext;
+
+import java.util.List;
+
+import kotlin.Unit;
+import kotlin.jvm.functions.Function0;
+import kotlin.jvm.functions.Function1;
 
 public class ArtInAreaActivity extends AppCompatActivity {
 
-    Button TestButton;
+    static final String APPID = "museum4all-f8y";
+    static final String APPTOKEN = "40a915a4bb95894fb98fa382eef15c9c";
+
+    private ProximityObserver proximityObserver;
+    ProximityZone zone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_art_in_area);
 
-        TestButton = (Button) findViewById(R.id.button);
-        TestButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(com.example.gudrun.test.ArtInAreaActivity.this, "I am here", Toast.LENGTH_SHORT).show();
+        EstimoteCloudCredentials cloudCredentials = new EstimoteCloudCredentials(APPID, APPTOKEN);
 
-                String url = "http://museum4all.integriert-studieren.jku.at/artefacts";
+        this.proximityObserver =
+                new ProximityObserverBuilder(getApplicationContext(), cloudCredentials)
+                        .onError(new Function1<Throwable, Unit>() {
+                            @Override
+                            public Unit invoke(Throwable throwable) {
+                                Log.e("app", "proximity observer error: " + throwable);
+                                return null;
+                            }
+                        })
+                        .withBalancedPowerMode()
+                        .build();
 
-                OkHttpClient client = new OkHttpClient();
-                okhttp3.Request request = new okhttp3.Request.Builder().url(url).build();
-                client.newCall(request).enqueue(new Callback() {
+         zone = new ProximityZoneBuilder()
+                .forTag("artefact")
+                .inNearRange()
+                .onEnter(new Function1<ProximityZoneContext, Unit>() {
                     @Override
-                    public void onFailure(Call call, IOException e) {
-                        e.printStackTrace();
+                    public Unit invoke(ProximityZoneContext context) {
+                        String title = context.getAttachments().get("title");
+                        Log.d("app", "This is the artefact " + title);
+                        return null;
                     }
-
+                })
+                .onExit(new Function1<ProximityZoneContext, Unit>() {
                     @Override
-                    public void onResponse(Call call, okhttp3.Response response) throws IOException {
-                        if (response.isSuccessful()) {
-                            String myResponse = response.body().string();
-                            System.out.println(myResponse);
-                        }
+                    public Unit invoke(ProximityZoneContext context) {
+                        Log.d("app", "Artefact not in range anymore");
+                        return null;
                     }
-                });
-            }
-        });
+                })
+                .build();
+
+        RequirementsWizardFactory
+                .createEstimoteRequirementsWizard()
+                .fulfillRequirements(this,
+                        // onRequirementsFulfilled
+                        new Function0<Unit>() {
+                            @Override public Unit invoke() {
+                                Log.d("app", "requirements fulfilled");
+                                proximityObserver.startObserving(zone);
+                                return null;
+                            }
+                        },
+                        // onRequirementsMissing
+                        new Function1<List<? extends Requirement>, Unit>() {
+                            @Override public Unit invoke(List<? extends Requirement> requirements) {
+                                Log.e("app", "requirements missing: " + requirements);
+                                return null;
+                            }
+                        },
+                        // onError
+                        new Function1<Throwable, Unit>() {
+                            @Override public Unit invoke(Throwable throwable) {
+                                Log.e("app", "requirements error: " + throwable);
+                                return null;
+                            }
+                        });
+
     }
 }
