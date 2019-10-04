0b09bdef2f0ebd146739bbf840a732e79990ceeb
Gudrun <Gudrun.huszar@gmx.at>
2019-10-04 08:26:33 +0200

    Find beacons with major

--- app/src/main/java/com/example/gudrun/test/ArtInAreaActivity.java
+++ app/src/main/java/com/example/gudrun/test/ArtInAreaActivity.java
@@ -1,98 +1,102 @@
 package com.example.gudrun.test;
 
-
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.estimote.coresdk.common.requirements.SystemRequirementsChecker;
+import com.estimote.coresdk.observation.region.beacon.BeaconRegion;
+import com.estimote.coresdk.recognition.packets.Beacon;
 
 
-import com.estimote.mustard.rx_goodness.rx_requirements_wizard.Requirement;
-import com.estimote.mustard.rx_goodness.rx_requirements_wizard.RequirementsWizardFactory;
-import com.estimote.proximity_sdk.api.EstimoteCloudCredentials;
-import com.estimote.proximity_sdk.api.ProximityObserver;
-import com.estimote.proximity_sdk.api.ProximityObserverBuilder;
-import com.estimote.proximity_sdk.api.ProximityZone;
-import com.estimote.proximity_sdk.api.ProximityZoneBuilder;
-import com.estimote.proximity_sdk.api.ProximityZoneContext;
-
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
-import kotlin.Unit;
-import kotlin.jvm.functions.Function0;
-import kotlin.jvm.functions.Function1;
+import com.estimote.coresdk.service.BeaconManager;
 
 public class ArtInAreaActivity extends AppCompatActivity {
 
     static final String APPID = "museum4all-f8y";
     static final String APPTOKEN = "40a915a4bb95894fb98fa382eef15c9c";
 
-    private ProximityObserver proximityObserver;
-    ProximityZone zone;
+    private BeaconManager beaconManager;
+    private BeaconRegion regionMint, regionBlue;
+
+    ListAdapterView adapter;
+    ListView artList;
+    ArrayList<String> listItems = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_art_in_area);
 
-        EstimoteCloudCredentials cloudCredentials = new EstimoteCloudCredentials(APPID, APPTOKEN);
+        final Button button = findViewById(R.id.button);
+        final TextView debug = findViewById(R.id.debug);
+        artList = (ListView) findViewById(R.id.artList);
 
-        this.proximityObserver =
-                new ProximityObserverBuilder(getApplicationContext(), cloudCredentials)
-                        .onError(new Function1<Throwable, Unit>() {
-                            @Override
-                            public Unit invoke(Throwable throwable) {
-                                Log.e("app", "proximity observer error: " + throwable);
-                                return null;
-                            }
-                        })
-                        .withBalancedPowerMode()
-                        .build();
 
-         zone = new ProximityZoneBuilder()
-                .forTag("artefact")
-                .inNearRange()
-                .onEnter(new Function1<ProximityZoneContext, Unit>() {
-                    @Override
-                    public Unit invoke(ProximityZoneContext context) {
-                        String title = context.getAttachments().get("title");
-                        Log.d("app", "This is the artefact " + title);
-                        return null;
-                    }
-                })
-                .onExit(new Function1<ProximityZoneContext, Unit>() {
-                    @Override
-                    public Unit invoke(ProximityZoneContext context) {
-                        Log.d("app", "Artefact not in range anymore");
-                        return null;
-                    }
-                })
-                .build();
+        debug.setText("No Beacons found so far");
 
-        RequirementsWizardFactory
-                .createEstimoteRequirementsWizard()
-                .fulfillRequirements(this,
-                        // onRequirementsFulfilled
-                        new Function0<Unit>() {
-                            @Override public Unit invoke() {
-                                Log.d("app", "requirements fulfilled");
-                                proximityObserver.startObserving(zone);
-                                return null;
-                            }
-                        },
-                        // onRequirementsMissing
-                        new Function1<List<? extends Requirement>, Unit>() {
-                            @Override public Unit invoke(List<? extends Requirement> requirements) {
-                                Log.e("app", "requirements missing: " + requirements);
-                                return null;
-                            }
-                        },
-                        // onError
-                        new Function1<Throwable, Unit>() {
-                            @Override public Unit invoke(Throwable throwable) {
-                                Log.e("app", "requirements error: " + throwable);
-                                return null;
+        beaconManager = new BeaconManager(this);
+        regionMint = new BeaconRegion("regionMint",
+                UUID.fromString("B9407F30-F5F8-466E-AFF9-25556B57FE6D"), null, null);
+        //regionBlue = new BeaconRegion("regionBlue", UUID.fromString("B9407F30-F5F8-466E-AFF9-25556B57FE6D"), null, null);
+
+        //TODO get the major. Make a table (key value pair) key is the major, value is the title
+        beaconManager.setRangingListener(new BeaconManager.BeaconRangingListener() {
+
+            @Override
+            public void onBeaconsDiscovered(BeaconRegion region, List<Beacon> list) {
+                if (!list.isEmpty()) {
+
+                    Log.d("Beacon found", "Count" + list.size() );
+                    debug.setText("Beacons found: " + list.size());
+                    // Show list of artefacts found
+                    //TODO match Major with artefacts.
+                    for (int i = 0; i < list.size(); i ++) {
+                        Beacon b = list.get(i);
+                        listItems.add(Integer.toString(b.getMajor()));
+                        ArtInAreaActivity.this.runOnUiThread(new Runnable() {
+                            public void run() {
+                                adapter = new ListAdapterView(ArtInAreaActivity.this, listItems);
+                                artList.setAdapter(adapter);
                             }
                         });
+                        if (b.getMajor() == 62122) {
+                            System.out.println("This is Monet");
+                        }
+                        if (b.getMajor() == 2) {
+                            System.out.println("This is the tree art");
+                        }
+                    }
+                }
+            }
+        });
 
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        SystemRequirementsChecker.checkWithDefaultDialogs(this);
+
+        beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
+            @Override
+            public void onServiceReady() {
+                beaconManager.startRanging(regionMint);
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        beaconManager.stopRanging(regionMint);
+        super.onPause();
     }
 }
--- app/src/main/java/com/example/gudrun/test/ArtInMuseum.java
+++ app/src/main/java/com/example/gudrun/test/ArtInMuseum.java
@@ -5,8 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
+
 import android.widget.ListView;
 import android.widget.Toast;
 import org.json.JSONException;
