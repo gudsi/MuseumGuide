4bc3a70063e45c1422c706caacf8c7050b063299
Gudrun <Gudrun.huszar@gmx.at>
2019-10-20 17:02:34 +0200

    Creating list of beacons in area To do: Show Artefact

--- app/src/main/java/com/example/gudrun/test/ArtInAreaActivity.java
+++ app/src/main/java/com/example/gudrun/test/ArtInAreaActivity.java
@@ -1,29 +1,29 @@
 package com.example.gudrun.test;
 
-import android.os.Build;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Html;
-import android.util.Base64;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.estimote.coresdk.common.requirements.SystemRequirementsChecker;
 import com.estimote.coresdk.observation.region.beacon.BeaconRegion;
 import com.estimote.coresdk.recognition.packets.Beacon;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 import com.estimote.coresdk.service.BeaconManager;
-import com.squareup.picasso.Picasso;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -44,9 +44,11 @@ public class ArtInAreaActivity extends AppCompatActivity {
     LinkedHashSet<String> listItems = new LinkedHashSet<>();
     //ArrayList<String> listItems = new ArrayList<>();
     String artefacts;
-    ArrayList<Integer> beacons = new ArrayList<>();
+    ArrayList<Integer> beaconsOfHTTPRequest = new ArrayList<>();
 
-    String nodeID;
+    Map<String, String> beaconsMajorToBID =  new HashMap<String, String>();
+
+    String nodeId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,15 +60,17 @@ public class ArtInAreaActivity extends AppCompatActivity {
         artList = (ListView) findViewById(R.id.artList);
         artefacts = getIntent().getStringExtra("artefacts");
 
+
+
         try {
             JSONObject description = null;
             description = new JSONObject(artefacts).getJSONObject("artefacts");
             Iterator<String> iterator = description.keys();
             while (iterator.hasNext()) {
-                String nodeID = iterator.next();
-                Object tmp = description.getJSONObject(nodeID);
+                String node = iterator.next();
+                Object tmp = description.getJSONObject(node);
                 String beaconID = ((JSONObject) tmp).getString("beacon_id");
-                beacons.add(Integer.parseInt(beaconID));
+                beaconsOfHTTPRequest.add(Integer.parseInt(beaconID));
             }
         } catch (JSONException e) {
             e.printStackTrace();
@@ -75,6 +79,7 @@ public class ArtInAreaActivity extends AppCompatActivity {
         debug.setText("No Beacons found so far");
 
         beaconManager = new BeaconManager(this);
+        beaconManager.setForegroundScanPeriod(1000, 0);
         regionMint = new BeaconRegion("regionMint",
                 UUID.fromString("B9407F30-F5F8-466E-AFF9-25556B57FE6D"), null, null);
 
@@ -82,66 +87,81 @@ public class ArtInAreaActivity extends AppCompatActivity {
 
             @Override
             public void onBeaconsDiscovered(BeaconRegion region, List<Beacon> list) {
+
                 if (!list.isEmpty()) {
+                    extractMapNodeIDToBID(list);
                     Log.d("Beacon found", "Count" + list.size() );
                     debug.setText("Beacons found: " + list.size());
                     // Show list of artefacts found
-                    //TODO show short_desc
                     for (int i = 0; i < list.size(); i ++) {
-                        Beacon b = list.get(i);
-                        listItems.add(Integer.toString(b.getMajor()));
-                        final List<String> myList = new ArrayList<>(listItems);
-                        ArtInAreaActivity.this.runOnUiThread(new Runnable() {
+                        Beacon beaconInRange = list.get(i);
+                        int major = beaconInRange.getMajor();
+                        nodeId = beaconsMajorToBID.get(Integer.toString(major));
+                        final NetworkAsyncTask httpTask = new NetworkAsyncTask(url + "/" + nodeId);
+                        httpTask.execute();
+                        new Thread(new Runnable() {
+                            @Override
                             public void run() {
-                                adapter = new ListAdapterView(ArtInAreaActivity.this, myList);
-                                artList.setAdapter(adapter);
+
+                                try {
+                                    final Object response = httpTask.get();
+                                    String myResponse = response.toString();
+                                    JSONObject description = null;
+                                    try {
+                                        description = new JSONObject(myResponse).getJSONObject(nodeId);
+                                        listItems.add(description.getString("short_desc"));
+                                        final List<String> myList = new ArrayList<>(listItems);
+                                        ArtInAreaActivity.this.runOnUiThread(new Runnable() {
+                                            public void run() {
+                                                adapter = new ListAdapterView(ArtInAreaActivity.this, myList);
+                                                artList.setAdapter(adapter);
+                                            }
+                                        });
+                                        System.out.println("Das ist: " + description.getString("short_desc") + " beacon_id: " + description.getString("beacon_id") + " node Id " + nodeId);
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }).start();
+
+                        artList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                            @Override
+                            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                                Toast.makeText(getApplicationContext(), "Item Clicked:" + i, Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(getApplicationContext(), ShowArtefactActivity.class);
+                                //TODO nodeID is null in showArtefact
+                                intent.putExtra("artefact", beaconsMajorToBID.get(nodeId));
+                                System.out.println(intent.getStringExtra("artefact"));
+                                startActivity(intent);
                             }
                         });
-                        if (beacons.contains(b.getMajor())) {
-                            String name ="";
-                            try {
-                                JSONObject description = null;
-                                description = new JSONObject(artefacts).getJSONObject("artefacts");
-                                Iterator<String> iterator = description.keys();
-                                while (iterator.hasNext()) {
-                                    nodeID = iterator.next();
-                                    Object tmp = description.getJSONObject(nodeID);
-                                    String id;
-                                    id = ((JSONObject) tmp).getString("beacon_id");
-                                    if(Integer.parseInt(id) == b.getMajor()) {
-                                        final NetworkAsyncTask httpTask = new NetworkAsyncTask(url + "/" + nodeID);
-                                        httpTask.execute();
-                                        new Thread(new Runnable() {
-                                            @Override
-                                            public void run() {
-                                                try {
-                                                    final Object response = httpTask.get();
-                                                    String myResponse = response.toString();
-                                                    System.out.println(myResponse);
-                                                    JSONObject description = null;
-                                                    try {
-                                                        description = new JSONObject(myResponse).getJSONObject(nodeID.toString());
-                                                        System.out.println("Das ist: " + description.getString("short_desc") + " beacon_id: " + description.getString("beacon_id") + " node Id " + nodeID);
-                                                    } catch (JSONException e) {
-                                                        e.printStackTrace();
-                                                    }
-                                                } catch (Exception e) {
-                                                    e.printStackTrace();
-                                                }
-                                            }
-                                        }).start();
-                                    }
-                                }
-                                System.out.println(name);
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
-                        }
                     }
                 }
             }
         });
 
+    }
+
+    private void extractMapNodeIDToBID(List<Beacon> list) {
+        String nodeIterator;
+        try {
+            //get information of each artefact
+            JSONObject description = null;
+            description = new JSONObject(artefacts).getJSONObject("artefacts");
+            Iterator<String> iterator = description.keys();
+            while (iterator.hasNext()) {
+                nodeIterator = iterator.next();
+                Object tmp = description.getJSONObject(nodeIterator);
+                String id;
+                id = ((JSONObject) tmp).getString("beacon_id");
+                beaconsMajorToBID.put(id, nodeIterator);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
